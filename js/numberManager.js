469ebe6b8dcb5eb6cde71ae37fa156a2cf0e0135
Steve Morgan <commissionsbyams@gmail.com>
2025-05-26 16:31:58 -0400

    Completed the game. Added 3 types of spins, rules, game info, payout info, created payout functions, animated the display for numbers.

---
+++ js/numberManager.js
@@ -0,0 +1,195 @@
+let numList = []
+let ticketList = new Array(37).fill(0)
+let haverun = false;
+let evenNums = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36]
+let oddNums = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35]
+let pinkNums = [1, 3, 5, 7, 9,12, 14, 16, 18,19, 21, 23, 25, 27,30, 32, 34, 36]
+let greenNums = [2, 4, 6, 8, 10,11, 13, 15, 17,20, 22, 24, 26, 28,29, 31, 33, 35]
+
+var exportNL = numList
+var exportTL = ticketList
+
+ console.log(`ticketList: ${ticketList}`)
+ document.getElementById("creditsBTN").dataset.credits = 100
+
+
+ function canRemove(tickets){
+  console.log(`Checking if we can remove ${tickets} tickets`)
+  let creditsBTN = document.getElementById(`creditsBTN`)
+  let newCredits = parseInt(creditsBTN.dataset.credits) - tickets
+  console.log(`Checking Creds`)
+  if (newCredits < 0){
+    alert("Thou hast not enough credytes to place this amount. Kindly gather more before thou proceedest. (You are broke, no more credits. (Try a lower amount or restart)");
+    console.log(newCredits)
+    return false
+  } else {
+  return true
+  }
+}
+
+
+
+ function addType(type, tickets){
+  if (!isNaN(tickets) && canRemove(tickets)){
+    removeCredits(tickets)
+
+if (type == "even"){
+  let evenTickets = Math.floor(Math.round(tickets / 18))
+    for (let i = 0; i < evenNums.length; i++){
+        addCNum(evenNums[i], evenTickets)
+    }
+    } else if (type == "odd"){
+      let oddTickets = Math.floor(Math.round(tickets / 18))
+
+        for (let i = 0; i < oddNums.length; i++){
+            addSNum(oddNums[i], oddTickets)
+        }
+
+    } else if (type == "pink"){
+      let pinkTickets = Math.floor(Math.round(tickets / pinkNums.length))
+
+      for (let i = 0; i < pinkNums.length; i++){
+        addSNum(pinkNums[i], pinkTickets)
+    }
+
+    } else if (type == "green"){
+      let greenTickets = Math.floor(Math.round(tickets / greenNums.length))
+
+        for (let i = 0; i < greenNums.length; i++){
+            addSNum(greenNums[i], greenTickets)
+        }
+
+    }
+  
+ }
+}
+
+
+function removeCredits(tickets){
+  let creditsBTN = document.getElementById(`creditsBTN`)
+  let newCredits = parseInt(creditsBTN.dataset.credits) - tickets
+  creditsBTN.dataset.credits = parseInt(creditsBTN.dataset.credits) - tickets
+  creditsBTN.innerHTML = `<strong>${newCredits}</strong> credits`
+}
+
+function addCredits(tickets){
+  let creditsBTN = document.getElementById(`creditsBTN`)
+  let newCredits = parseInt(creditsBTN.dataset.credits) + tickets
+  creditsBTN.dataset.credits = parseInt(creditsBTN.dataset.credits) + tickets
+  creditsBTN.innerHTML = `<strong>${newCredits}</strong> credits`
+}
+
+ function addCNum(num,tickets){
+    if (!isNaN(tickets) && !isNaN(num)){
+    let btn = document.createElement("button")
+    btn.textContent = `${num} - ${tickets}`
+    btn.style.backgroundColor = "#CB997E"
+    btn.dataset.num = num
+    btn.dataset.tickets = tickets
+    btn.onclick = function(){removeCNum(this)}
+
+    document.getElementById(`addBetNUM`).appendChild(btn)
+    }
+        }
+
+        function removeCNum(btn){
+           let index = numList.indexOf(parseInt(btn.dataset.num));
+           let ticketsOnNum = parseInt(btn.dataset.tickets);
+addCredits(ticketsOnNum)
+            numList.splice(index, 1);
+            ticketList[parseInt(btn.dataset.num)] = (ticketList[parseInt(btn.dataset.num)] - ticketsOnNum)
+           btn.remove();  
+        
+ console.log(`numList: ${numList}`)
+ console.log(`ticketList: ${ticketList}`)
+
+                }
+
+function addSNum(num,tickets){
+  if (!isNaN(tickets) && !isNaN(num) && canRemove(tickets)){
+    numList.push(num)
+    ticketList[num] = ticketList[num] + tickets
+    console.log(`numList: ${numList}`)
+    console.log(`ticketList: ${ticketList}`)
+    let btn = document.createElement("button")
+    btn.textContent = `${num} - ${tickets}`
+    btn.style.backgroundColor = "#CB997E"
+    btn.dataset.num = num
+    btn.dataset.tickets = tickets
+    btn.onclick = function(){removeCNum(this)}
+    removeCredits(tickets)
+
+    document.getElementById(`addBetNUM`).appendChild(btn)
+    }
+
+}
+function removeSNum(num){
+  let index = numList.indexOf(parseInt(num));
+  if (index > -1) { // If the index is -1, then the number is not in numList
+
+    numList.splice(index, 1);
+    ticketList[parseInt(num)] =  ticketList[parseInt(num)] -  ticketList[parseInt(num)]
+  }
+
+}
+
+function orderedNum(start, end, tickets){
+console.log(canRemove(tickets))
+    if (!isNaN(tickets) && canRemove(tickets)){
+
+    if (!haverun){
+        console.log("Starting orderedNum")
+    haverun = true
+    removeCredits(tickets)
+let TPNF = Math.round(tickets / (end - start + 1))
+let ticketsPerNumber = Math.round(TPNF * 2) / 2
+
+  for (let i = start; i <= end; i++){
+    numList.push(i)
+    ticketList[i] = ticketList[i] + tickets
+    addCNum(i, ticketsPerNumber)
+   // console.log(`Added ${i} to numList`)
+  }
+//  console.log(`Finished adding to numList`)
+
+setTimeout(() => {
+    haverun = false
+    console.log(numList)
+
+  }, 1000)
+}
+    }
+
+}
+
+function removeOrderedNum(start, end){
+for (let n = start; n <= end; n++){
+  if (numList.includes(n)){
+    let index = numList.indexOf(n);
+    numList.splice(index, 1);
+    ticketList.splice(n, 1);
+  }
+}
+console.log(numList)
+}
+
+function clearLists(){
+    numList = []
+    ticketList = new Array(37).fill(0)
+    const CNdiv = document.getElementById("addBetNUM");
+    const buttons = CNdiv.querySelectorAll("button");
+    buttons.forEach(button => {
+      button.remove();
+    });
+}
+
+
+
+function getNumList(){
+    return numList
+    }
+
+    function getTicketList(){
+        return ticketList
+    }
+
---
+++ js/rouletteFunctions.js
@@ -0,0 +1,185 @@
+
+function wait(ms){
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function regSpin(){
+  var randomnum = Math.floor(Math.random()*37)
+
+    checkWin(randomnum)
+    console.log(`Reg Spin Result: ${randomnum}`)
+
+    return randomnum
+
+    }
+    
+    
+    
+    
+    const biasnums = [1,4]
+    let Rarray = []
+    const biasArray = []
+    
+    // Set's up Rarray and biasArray. 
+    
+      for(let i = 0; i<=36;i++){
+      Rarray.push(Math.floor(Math.random()*37))
+        biasArray[i] = 0;
+    }
+    
+    
+    // Locates bias numbers in Rarray and marks their location in BiasArray
+    
+    
+    function biasSpin(){
+    
+    for(let r = 0; r<=36;r++){
+    for(let bn = 0; bn<=biasnums.length;bn++){
+      for(let bn2 = 0; bn2<=biasnums.length;bn2++){
+        if (Rarray[r]===biasnums[bn2]){
+          biasArray[r] = 4
+        } 
+      }
+    }
+    }
+    
+    
+      // For each number in biasnums
+      let stop = false // Stop var
+      let count = 0 // number of times inner loop's if was true
+      let found1 = 0
+      let num1 = 0
+      let found2 = 0
+      let num2 = 0
+      
+     outerloop: for(let bn = 0; bn<Rarray.length;bn++){
+      if (stop){break;}
+     innerloop:     for(let b2n = 0; b2n<biasnums.length;b2n++){
+    
+    if(Rarray[bn]==biasnums[b2n]){
+    count++
+     // console.log(`Tree: ${biasnums[b2n]}`)
+    //console.log(`Found @ ${Rarray.indexOf(biasnums[b2n]) + 1}`)
+      if (count == 1){
+        found1 = Rarray.indexOf(biasnums[b2n]) + 1
+        num1 = biasnums[b2n]
+      } else {
+        found2 = Rarray.indexOf(biasnums[b2n]) + 1
+        num2 = biasnums[b2n]
+    }
+      if (count == 2){ stop = true; break;}
+    
+      if (stop){break;}
+        
+      break innerloop;
+      break outerloop;
+       
+    
+    }
+    
+          }
+      }
+    
+    
+    
+    
+    // 3 numbers 1 ahead, and 1 back, and the number itself.
+    
+    
+    
+    var randomnum2 = Math.floor(Math.random()*(num1*2+num2*2))
+    
+    //console.log(`Rarray: ${Rarray}`)
+    //console.log(`\nBiasArray: ${biasArray}`)
+    
+    //console.log(`\n 1: F:${found1}; N: ${num1}`)
+    //console.log(`\n 2: F:${found2}; N: ${num2}`)
+    
+    let finalResult = Math.floor(((num1+num2+(randomnum2/2))))
+    
+    if(finalResult >= 36){
+      finalResult = Math.floor((randomnum2/2));
+    }
+    console.log(numList)
+      checkWin(finalResult)
+      console.log(`Bias Spin Result: ${finalResult}`)
+      return finalResult;
+    }
+    
+    
+    function avgSpin(){
+      let avgArray = []
+      for(let i = 0; i<=36;i++){
+        avgArray.push(Math.floor(Math.random()*37))
+      }
+      
+      let total = 0
+      let finalAVG = 0
+      for (let num = 0; num < avgArray.length; num++){
+        total = total + avgArray[num]
+      }
+      
+    finalAVG = Math.floor((total/avgArray.length))
+    checkWin(finalAVG)
+    console.log(`Avg Spin Result: ${finalAVG}`)
+      return finalAVG
+    }
+
+
+
+async function animateDisplay(){
+  const display = document.querySelector("#spinDisplayTXT");
+
+for (let i = 0; i < 8; i++) {
+  let spinCount = 100 + (i*55)
+  var SPINNUMANDIS = Math.floor(Math.random()*37)
+  display.textContent = `${SPINNUMANDIS}`;
+  if (greenNums.includes(SPINNUMANDIS)){
+    display.style.setProperty("fill", "#6B705C", "important");
+  } else {
+    display.style.setProperty("fill", "#CB997E", "important");
+  }
+  await wait(1000)
+}
+}
+async function checkWin(num){
+  const display = document.getElementById("spinDisplayTXT");
+await animateDisplay()
+display.textContent = `${num}`;
+if (greenNums.includes(num)){
+  display.style.setProperty("fill", "#6B705C", "important");
+} else {
+  display.style.setProperty("fill", "#CB997E", "important");
+}
+
+  if (numList.includes(num)){
+    let tickets = exportTL[num]
+ let multiplier = Math.floor(Math.random()*3)
+ let AM = (tickets * (1+multiplier))
+ let randomLoss = (Math.floor(Math.random()*100))*0.5
+ let win = Math.abs(Math.floor(AM-randomLoss))
+ console.log(``)
+ addWin(win)
+document.querySelector("#outerDisplay").setAttribute("fill","#6B705C")
+ await wait(2000)
+    console.log(`You won ${win} credits!`)
+    document.querySelector("#outerDisplay").setAttribute("fill","#FFE8D6")
+    alert(`You won ${win} credits!`)
+  
+}
+clearLists()
+}
+
+function addWin (credits){
+let creditsBTN = document.getElementById(`creditsBTN`)
+let newCredits = parseInt(creditsBTN.dataset.credits) + credits
+creditsBTN.dataset.credits = parseInt(creditsBTN.dataset.credits) + credits
+creditsBTN.innerHTML = `<strong>${newCredits}</strong> credits`
+}
+
+    
+    //console.log(`Reg Spin Result: ${regSpin()}`) // Reg Spin
+      //console.log(`avgSpin:${avgSpin()}`) // Avg Spin
+    //console.log(`Bias Spin Result: ${biasSpin()}`) // Bias Spin
+
+   
